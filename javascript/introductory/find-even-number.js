e776487d77e5e6a489df943eedb95bbdad0a67fb
knowave <kwaktaemin98@gmail.com>
2024-03-08 16:28:18 +0900

    짝수는 싫어요

---
+++ javascript/introductory/find-even-number.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    const answer = [];
+
+    for (let i = 0; i <= n; i++)  {
+        if (i % 2 !== 0) {
+            answer.push(i);
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution(15));
