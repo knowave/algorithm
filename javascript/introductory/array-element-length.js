48ad9a8bc2f56551bfbe57d3b1a02e301e50f567
knowave <kwaktaemin98@gmail.com>
2024-03-22 15:09:48 +0900

    배열 원소의 길이

--- javascript/introductory/array-element-length.js
+++ javascript/introductory/array-element-length.js
@@ -1,9 +1,8 @@
 function solution(strlist) {
     let answer = [];
 
-    for (let i = 0; i < strlist.length; i++) {
-        const strSplit = strlist[i].split('');
-        answer.push(strSplit.length);
+    for (const str of strlist) {
+        answer.push(str.length);
     }
 
     return answer;
