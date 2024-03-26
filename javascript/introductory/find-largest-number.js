80889e1b43897668594b17542f1a0fd697376b98
knowave <kwaktaemin98@gmail.com>
2024-03-26 16:15:16 +0900

    가장 큰 수 찾기

---
+++ javascript/introductory/find-largest-number.js
@@ -0,0 +1,8 @@
+function solution(array) {
+    const max = Math.max(...array);
+
+    return [max, array.indexOf(max)];
+}
+
+console.log(solution([1, 8, 3]));
+console.log(solution([9, 10, 11, 8]));
