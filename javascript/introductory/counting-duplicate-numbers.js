754daa1deb90c119dfa251115637b4c6b7f282ec
knowave <kwaktaemin98@gmail.com>
2024-04-01 16:15:20 +0900

    중복된 숫자 세기

--- javascript/introductory/counting-duplicate-numbers.js
+++ javascript/introductory/counting-duplicate-numbers.js
@@ -1,12 +1,3 @@
 function solution(array, n) {
-    let answer = 0;
-
-    for (const el of array) {
-        if (el === n) answer ++;
-    }
-
-    return answer;
+    return array.filter((el) => el === n).length;
 }
-
-console.log(solution([1, 1, 2, 3, 4, 5], 1)) // 2
-console.log(solution([0, 2, 3, 4], 1)) // 2
