156c028e1f7295b78ca3d6fd152e64d56743408c
knowave <kwaktaemin98@gmail.com>
2024-04-22 13:53:24 +0900

    평행

--- javascript/introductory/parallel.js
+++ javascript/introductory/parallel.js
@@ -1,33 +1,13 @@
-function isParallel(dot1, dot2, dot3, dot4) {
-    const slope1 = (dot2[1] - dot1[1]) / (dot2[0] - dot1[0]);
-    const slope2 = (dot4[1] - dot3[1]) / (dot4[0] - dot3[0]);
-
-    return slope1 === slope2;
-}
-
 function solution(dots) {
-    // 가능한 직선 조합 생성
-    const lines = [];
-    for (let i = 0; i < dots.length; i++) {
-        for (let j = i + 1; j < dots.length; j++) {
-            lines.push([dots[i], dots[j]]);
-        }
-    }
-
-    // 평행한 직선이 있는지 확인
-    for (let i = 0; i < lines.length; i++) {
-        for (let j = i + 1; j < lines.length; j++) {
-            const line1 = lines[i];
-            const line2 = lines[j];
-            if (isParallel(line1[0], line1[1], line2[0], line2[1])) {
-                return 1;
-            }
-        }
-    }
-
+    if (calculateSlope(dots[0], dots[1]) === calculateSlope(dots[2], dots[3]))
+        return 1;
+    if (calculateSlope(dots[0], dots[2]) === calculateSlope(dots[1], dots[3]))
+        return 1;
+    if (calculateSlope(dots[0], dots[3]) === calculateSlope(dots[1], dots[2]))
+        return 1;
     return 0;
 }
 
-// 테스트 케이스
-console.log(solution([[1, 4], [9, 2], [3, 8], [11, 6]])); // 1
-console.log(solution([[3, 5], [4, 1], [2, 4], [5, 10]])); // 0
+function calculateSlope(arr1, arr2) {
+    return (arr2[1] - arr1[1]) / (arr2[0] - arr1[0]);
+}
